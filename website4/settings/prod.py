d31461efb13d61be9a8f0a4d9c7ea975b149c767
syhanjin <2819469337@qq.com>
2022-07-14 16:52:44 +0800

    0.1.2-fix4

--- website4/settings/prod.py
+++ website4/settings/prod.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 # ==============================================================================
 #  Copyright (C) 2022 Sakuyark, Inc. All Rights Reserved                       =
 #                                                                              =
-#    @Time : 2022-7-14 16:48                                                   =
+#    @Time : 2022-7-14 16:52                                                   =
 #    @Author : hanjin                                                          =
 #    @Email : 2819469337@qq.com                                                =
 #    @File : prod.py                                                           =
@@ -199,6 +199,6 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_FROM = "Sakuyark"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 BASE_URL = "api/v1/"
