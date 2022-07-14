057277f97c08d566dad0837529cbb452be1ed42a
syhanjin <2819469337@qq.com>
2022-07-14 16:43:51 +0800

    0.1.2-fix

--- website4/wsgi.py
+++ website4/wsgi.py
@@ -10,17 +10,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 # ==============================================================================
 #  Copyright (C) 2022 Sakuyark, Inc. All Rights Reserved                       =
 #                                                                              =
-#    @Time : 2022-7-12 10:20                                                   =
+#    @Time : 2022-7-14 16:43                                                   =
 #    @Author : hanjin                                                          =
 #    @Email : 2819469337@qq.com                                                =
 #    @File : wsgi.py                                                           =
 #    @Program: website                                                         =
 # ==============================================================================
 
-import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website4.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website4.settings')
 
 application = get_wsgi_application()
